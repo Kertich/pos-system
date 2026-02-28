1edc6c841ca30495e4377bbc28fe88e02677e209
Kipsang Kertich <kipsangkertich@gmail.com>
2026-02-28 22:26:08 +0300

    chore: add mock data

---
+++ app/lib/mockData.ts
@@ -0,0 +1,9 @@
+import { Product } from "../types"
+
+// Mock data for products
+export const products: Product[] = [
+    {id: "1", name: "Coca Cola", price: 50},
+    {id: "2", name: "Bread", price: 60},
+    {id: "3", name: "Milk", price: 55},
+    {id: "4", name: "Sugar", price: 120},
+]
